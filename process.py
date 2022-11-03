e588addfd7cffe81f36de5e322045483cae752fd
DanilSukach <danil211-1@yandex.ru>
2022-11-03 11:40:32 +0400

    type-hinting

--- process.py
+++ process.py
@@ -2,17 +2,17 @@ from multiprocessing import Process
 import click
 
 
-def discriminant(a, b, c):
+def discriminant(a: int, b: int, c: int) -> int:
     D = b**2 - 4 * a * c
     return D
 
 
-def x1(a, b, c, D):
+def x1(a: int, b: int, c: int, D: int) -> float:
     x = (-b + D**0.5) / (2 * a)
     print("X1 = ", x)
 
 
-def x2(a, b, c, D):
+def x2(a: int, b: int, c: int, D: int) -> float:
     x = (-b - D**0.5) / (2 * a)
     print("X2 = ", x)
 
@@ -22,9 +22,9 @@ def x2(a, b, c, D):
 @click.argument("b", default=1)
 @click.argument("c", default=1)
 def main(a, b, c):
-    D = discriminant(int(a), int(b), int(c))
-    process1 = Process(target=x1, args=(int(a), int(b), int(c), D))
-    process2 = Process(target=x2, args=(int(a), int(b), int(c), D))
+    D = discriminant(a, b, c)
+    process1 = Process(target=x1, args=(a, b, c, D))
+    process2 = Process(target=x2, args=(a, b, c, D))
 
     process1.start()
     process2.start()
